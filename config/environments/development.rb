0f517d8f0c0fbcdc90e8bb80374d31021b51f137
rkbart <rk_bart@yahoo.com>
2025-11-14 16:37:38 +0800

    set tailwind css version to 3.4.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true
   }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
